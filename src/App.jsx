53fa1ee69652a4c895347617ce84d901ff83ebd5
manishfw13 <manish_fw13_347@masai.school>
2022-05-06 09:40:53 +0530

    navbar and fotter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import './App.css';
+import { Footer } from './components/Fotter/Footer';
+import { Navbar } from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+       <Navbar/>
+        <Footer/> 
+    </div>
+  );
+}
+
+export default App;
