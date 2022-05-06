2ccd8f04ff60392f446aff8294906b03bae22fd4
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-05-06 09:35:39 +0530

    Added product page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Productsmain } from './components/Products/Productsmain';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Productsmain/>
+    </div>
+  );
+}
+
+export default App;
