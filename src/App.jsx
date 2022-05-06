244afba9f0a7d81e871cd49e46060569c08aa9cd
manishfw13 <manish_fw13_347@masai.school>
2022-05-06 10:05:14 +0530

    some change in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
+import { Footer } from './components/Fotter/Footer';
+import { Navbar } from './components/Navbar/Navbar';
 import { Productsmain } from './components/Products/Productsmain';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Productsmain/>
+      <Footer/>
     </div>
   );
 }
