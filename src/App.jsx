2c31950e0466ff3f145630264dfda69f5264f2c4
manishfw13 <manish_fw13_347@masai.school>
2022-05-06 09:45:07 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
  
 import './App.css';
-import { Footer } from './components/Fotter/Footer';
-import { Navbar } from './components/Navbar/Navbar';
+ 
 
 function App() {
   return (
     <div className="App">
-       <Navbar/>
-        <Footer/> 
+     
     </div>
   );
 }
